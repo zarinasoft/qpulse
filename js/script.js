bd3694b3e7b94af9ebdc3e32605f6a0f73a3f17c
Mubashir Khan <codephantom404@gmail.com>
2025-01-25 08:45:48 +0500

    copyright

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@
 // document.getElementById("alertButton").addEventListener("click", function () {
 //     alert("Hello! You clicked the button.");
 // });
+
+const currentYear = new Date().getFullYear();
+// Set the year in the footer
+document.getElementById('currentYear').textContent = currentYear;
